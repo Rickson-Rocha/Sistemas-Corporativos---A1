f325b00d8127c93f9da76ea409a6fcf02e64bb4e
Cecília do Nascimento Miranda <20221148060012@ifrn.local>
2023-11-01 07:52:36 -0300

    adicionando modelo de livro

---
+++ src/main/java/com/ricksonrocha/books/model/livro/Livro.java
@@ -0,0 +1,43 @@
+package com.ricksonrocha.books.model.livro;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="livro")
+@Table(name="livro")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Livro {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    private String Isbn;
+    
+    @NotBlank
+    private LocalDate anoPubli;
+
+    @NotBlank
+    private Integer copiasDispo;
+
+    // private Usuario usuario;
+}
